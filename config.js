973a36bfdc87b0802b0a43708948c28e44b8eb60
Hatun20 <hatun2k.yt@gmail.com>
2022-03-29 23:47:31 +0700

    yeu

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Tiêu đề web",
+    introTitle: 'LELE',
+    introDesc: `Trái đất vốn lạ thường, 
+    Mà sao em cứ đi nhầm đường, 
+    Lạc vào tim anh lẻ loi, 
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^Haha^^',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối mai 7h, anh chở sang chị Phượng nha.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: '' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
